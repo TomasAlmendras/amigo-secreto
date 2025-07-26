c13c9ec0603192be5d08908896c9733c459817b9
Tomas Almendras <tomasalemendras@gmail.com>
2025-07-26 11:44:15 -0300

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
-// Aquí deberás desarrollar la lógica para resolver el problema.
-
 //Crear un array para almacenar los nombres
 let amigosIngresados = [];
 
 //Implementa una función para agregar amigos
-
 function agregarAmigo(){
 
     validarListaAmigo();
@@ -25,34 +21,5 @@ function actualizarAmigo(){
     });
 }
 
-//agregarAmigo();
-function sortearAmigo(){
-    let amigoSorteado;
-    if(amigosIngresados === ''){
-        alert(`Debe ingresar un amigo antes de sortear`);
-        validarListaAmigo();
-    }else{
-        amigoSorteado = Math.floor(Math.random(amigosIngresados.length)*amigosIngresados.length)+1;
-        
-        console.log(`indice del amigo sorteado: ${amigoSorteado}`);
-    }
-    
-}
 
-function limpiarCaja(){
-    document.getElementById('amigo').value = '';
-}
-
-function validarListaAmigo(){
-    if (document.getElementById('amigo').value === ""){
-        alert(`Por favor, inserte un nombre`);
-    }else{
-        amigosIngresados.push(document.getElementById('amigo').value);
-        limpiarCaja();
-    }
-}
-
-
-
-//Implementa una función para sortear los amigos
 
